1de902d7229050f9238088d009e720a5aac70f90
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-05 16:27:16 -0300

    iniciei a base do p5 e as divs onde ficarão os conteudos

---
+++ src/sketch.js
@@ -0,0 +1,15 @@
+
+
+  function setup() {
+    createCanvas(windowWidth*0.4, 500);
+  }
+  
+  function draw() {
+    // put draswing here
+    background(220);
+  }
+  
+  windowResized = function() {
+    resizeCanvas(windowWidth*0.4, 500);
+  };
+  
